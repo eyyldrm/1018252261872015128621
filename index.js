6244311f864f4a1e49b453e0c108efd6b61ab369
eyyldrm <eyyldrm.18@gmail.com>
2018-08-10 22:45:19 +0300

    f

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
   res.end("selamlar");
 });
 
 io.on('connection', function(socket){
     console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('broadcast', function(data){
+    console.log(data);
   });
 });
 
