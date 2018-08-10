897c40a3918317b6770d07a0be14ca2dc88f1c57
eyyldrm <eyyldrm.18@gmail.com>
2018-08-10 22:05:57 +0300

    k

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
 
-server.listen(3000);
-// WARNING: app.listen(80) will NOT work here!
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
-app.get('/', function (req, res) {
-  res.end('selamlar');
+app.get('/', function(req, res){
+    res.end("selamlar");
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
+
+
+app.listen(port, () => console.log('Port 3000 aktif hale getirildi.')); 
