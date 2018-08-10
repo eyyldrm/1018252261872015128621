34fd91ba369e679daf57b81780b4fb49102b9179
eyyldrm <eyyldrm.18@gmail.com>
2018-08-10 22:27:05 +0300

    şi

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+    console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(port, function(){
