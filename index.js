48631451cea518cedd300fc9a7aaf4563286fdc1
eyyldrm <eyyldrm.18@gmail.com>
2018-08-12 01:45:42 +0300

    f

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     console.log('a user connected');
   socket.on('pubgName', function(data){
-    var data = JSON.stringify(data);
-    console.log('User PUBG name is ' + data.pubgName);
+    console.log('User PUBG name is ' + data);
   });
 });
 
