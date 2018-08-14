19efb53d3f0422f9693263e40aecedb326a10fc9
eyyldrm <eyyldrm.18@gmail.com>
2018-08-14 19:05:23 +0300

    d

--- index.js
+++ index.js
@@ -243,6 +243,14 @@ io.on('connection', function(socket){
         }
       });
   });
+    socket.on('pubgStats', function(data){
+    var chatstr = JSON.stringify(data);
+    var chatParse = JSON.parse(chatstr);
+    var chatMassage  = chatParse.chatMassage;
+    var username = chatMassage.userName;
+
+    io.emit('chatMessageC', {chatMassages: chatMassage, userName: username});
+    });
 });
 
 http.listen(port, function(){
